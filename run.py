bd5812cb8fd2d3fdc963416f3b3c0cbb0ec5bd2f
JayKearney <indi.mh@gmail.com>
2021-11-12 21:17:46 +0000

    enter user input variable, select year and print results

--- run.py
+++ run.py
@@ -3,4 +3,18 @@ import prettytable as pt
 
 df = pd.read_csv('Top-Football-Leagues-Scorers.csv')
 
+# User input
+
+year = input('Enter a year from 2016 to 2020: ')
+year = int(year)
+
+# If value not accepted has been enter - error message and print again - else if
+
+#Output
+
+result = df[df['Year'] == year]
+print(result)
+
+#result = df[df['Player Names'] == 'Luis Suarez']
+#print(result)
 
